2744b6b351a10fc08b1d9f81adf6bb019a794eed
github username <github email address>
2019-09-24 17:32:32 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,9 @@
+
+
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+
   end
 
   def index
@@ -9,8 +12,31 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: 'zero')
+    @recipe.ingredients.build(quantity: 'zero')
   end
 
   def create
+    
+    Recipe.create(recipe_params)
+    
+    
+    # byebug
+    
   end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+        ]
+      )
+    end
+
+    def ingredient_params(*args)
+      params.require(:recipe).require(:ingredients_attributes).permit(*args)
+    end
 end
